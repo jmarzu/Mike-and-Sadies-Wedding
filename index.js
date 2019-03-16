e0d91efad4bf6da6ca118f86dfab95dde32c5f50
Joe Marzullo <jmarzu13@gmail.com>
2019-03-16 15:14:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var fs = require('fs');
+var path = require('path');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.get('/', function(rec, res) {
+	res.render('main');
+});
+
+app.get('/directions', function(rec, res) {
+	res.render('directions');
+});
+
+app.get('/reservations', function(rec, res) {
+	res.render('reservations');
+});
+
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
